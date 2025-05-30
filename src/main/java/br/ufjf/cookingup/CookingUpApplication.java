1fb8411c034792a10f998832b6da40ac6a9ffb4b
Celso Z. <celso.zacarias@estudante.ufjf.br>
2025-05-29 21:25:25 -0300

    Commit inicial

---
+++ src/main/java/br/ufjf/cookingup/CookingUpApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.cookingup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/cookingup/CookingUpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.cookingup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
